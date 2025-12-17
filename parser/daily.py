617d928c97502884cde54644e26a738018b60a99
kiss-my-classy <applyadoptsanddrow@gmail.com>
2025-12-17 19:39:22 +0300

    ver1.1 code parsed fully

--- parser/daily.py
+++ parser/daily.py
@@ -1,44 +1,38 @@
 from datetime import datetime
-from .time_utils import TZ, MONTHS_RU
+from .time_utils import TZ
 import os
 import json
+
+def get_data() -> dict:
+    raw = os.getenv("DAILY_JSON")
+    if not raw:
+        return {}
+    try:
+        return json.loads(raw)
+    except json.JSONDecodeError:
+        return {}
+
+def get_date() -> str:
+    return get_data().get("date", "")
+
+def get_tasks() -> list[dict]:
+    return get_data().get("tasks", [])
 
 def list_daily() -> list[str]:
     """
     Возвращает ежедневные задания, если дата в DAILY_JSON совпадает с текущей датой Sky (TZ)
     """
     date = get_date()
-    if date == "":
+    if not date:
         return []
 
     today = datetime.now(TZ).date().isoformat()
 
     if date != today:
         return []
-    
+
     return [
         task["text"]
         for task in get_tasks()
-            if isinstance(task, dict) and "text" in task
+        if isinstance(task, dict) and "text" in task
     ]
-
-def get_date() -> str:
-    data = os.getenv("DAILY_JSON")
-    if not data:
-        return ""
-
-    try:
-        return data.get("date")
-    except json.JSONDecodeError:
-        return ""
-
-    
-def get_tasks() -> list[str]:
-    data = os.getenv("DAILY_JSON")
-    if not data:
-        return []
-
-    try:
-        return data.get("tasks")
-    except json.JSONDecodeError:
-        return []
